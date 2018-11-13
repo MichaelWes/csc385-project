97669cd63bfed192ba5de8075260953546c4aa4a
Michael Wesolowski <wesolo18@teach.cs.toronto.edu>
2018-11-13 11:26:52 -0500

    attempt at HEX1 handler

--- setup_interrupts.s
+++ setup_interrupts.s
@@ -6,10 +6,21 @@
 .global setup_interrupts
 
 setup_interrupts:
+/* Turn everything off*/
+	movia r8, ADDR_JP1
+    movia r9, ADDR_PUSHB 
+    
+	movia r10, 0x7F557FF       # set direction for motors to all output 
+	stwio r10, 4(r8)
+  
+	movia r10, 0xFFFFFFFF       # turn off all motors.
+	stwio r10, 0(r8)
+/*     */
+
   movia r2,ADDR_PUSHB
-  movia r3,0x1	  # Enable interrrupt mask = 0001
+  movia r3,0x3	  # Enable interrrupt mask = 0011
   stwio r3,8(r2)  # Enable interrupts on pushbuttons 1,2, and 3
-  stwio r3,12(r2) # Clear edge capture register to prevent unexpected interrupt
+  stwio r0,12(r2) # Clear edge capture register to prevent unexpected interrupt
 
   movia r2,IRQ_PUSHBUTTONS
   wrctl ctl3,r2   # Enable bit 1 - Pushbuttons use IRQ 1
@@ -25,7 +36,7 @@ setup_interrupts:
     stw r9, 4(sp)
     stw r10, 8(sp)
     stw r11, 12(sp)
-	movia r23, 0x1234
+
 # Check if interrupt was caused by a device
     rdctl et, ipending
     beq et, r0, interrupt_epilogue
@@ -38,8 +49,18 @@ setup_interrupts:
     andi r9, et, 0x02 # IRQ 1
     beq r9, r0, interrupt_epilogue
 
+# If we get here, a HEX keypad button was pushed for sure.
+# Determine which HEX keypad button was pushed, and branch to its handler.
+    movia r9, ADDR_PUSHB 
+    ldwio r10, 12(r9)           # load edge capture register
+    addi r11, r0, 1
+
+    # bit 0 set -> HEX0
+    beq r10, r11, HEX0_handler
+    slli r11, r11, 1
+    beq r10, r11, HEX1_handler
+
 HEX0_handler:
-	movia r22, 0x5678
 	movia r8, ADDR_JP1
     movia r9, ADDR_PUSHB 
     
@@ -47,17 +68,26 @@ HEX0_handler:
 	stwio r10, 4(r8)
   
 	movia r10, 0xFFFFFFFC       # motor0 enabled (bit0=0), direction set to forward (bit1=0) 
-	stwio r10, 0(r8)  
+	stwio r10, 0(r8)
+    jmpi interrupt_epilogue  
+
+HEX1_handler:
+	movia r8, ADDR_JP1
+    movia r9, ADDR_PUSHB 
     
-    movia r11, 10000 
-loop:
-	addi r11, r11, -1
-	bgt r11, r0, loop
-    movia r10, 0xFFFFFFFF
-	stwio r10, 0(r8)  
-    
+	movia r10, 0x7F557FF       # set direction for motors to all output 
+	stwio r10, 4(r8)
+  
+	movia r10, 0xFFFFFFFF       # turn off all motors.
+	stwio r10, 0(r8)
+    jmpi interrupt_epilogue  
+
     
 interrupt_epilogue:
+    
+    movia et, ADDR_PUSHB
+    stwio r0, 12(et)            # clear HEX edge capture registers by write.
+
 	ldw r11, 12(sp)
     ldw r10, 8(sp)
     ldw r9, 4(sp)
