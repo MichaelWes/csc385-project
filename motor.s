4dcfa1db7b443dd22079b9a0bbf4b28051437721
0xHASEEB <haseeb.choudhary@mail.utoronto.ca>
2018-11-27 16:51:39 -0500

    Tabs to spaces.

--- motor.s
+++ motor.s
@@ -3,27 +3,27 @@
 .global motor0_bwd
 
 motors_off:
-	subi sp, sp, 8
-	stw r8, 0(sp)
-	stw r9, 4(sp)
-	
-	movia r8, ADDR_JP1
+    subi sp, sp, 8
+    stw r8, 0(sp)
+    stw r9, 4(sp)
+    
+    movia r8, ADDR_JP1
     movia r9, 0x7F557FF            # Set direction for motors to all output 
     stwio r9, JP1_DIRREG(r8)
 
     movia r9, 0xFFFFFFFF           # Turn off all motors.
     stwio r9, JP1_DATA(r8)
-	
-	ldw r9, 4(sp)
-	ldw r8, 0(sp)
-	addi sp, sp, 8
+    
+    ldw r9, 4(sp)
+    ldw r8, 0(sp)
+    addi sp, sp, 8
 	
     ret
             
 motor0_fwd:
-	subi sp, sp, 8
-	stw r8, 0(sp)
-	stw r9, 4(sp)
+    subi sp, sp, 8
+    stw r8, 0(sp)
+    stw r9, 4(sp)
 
     movia r8, ADDR_JP1
     movia r9, 0x7F557FF            
@@ -31,17 +31,17 @@ motor0_fwd:
 
     movia r9, 0xFFFFFFFC           
     stwio r9, JP1_DATA(r8)
-
-	ldw r9, 4(sp)
-	ldw r8, 0(sp)
-	addi sp, sp, 8
-	
-	ret
+    
+    ldw r9, 4(sp)
+    ldw r8, 0(sp)
+    addi sp, sp, 8
+    
+    ret
             
 motor0_bwd:
-	subi sp, sp, 8
-	stw r8, 0(sp)
-	stw r9, 4(sp)
+    subi sp, sp, 8
+    stw r8, 0(sp)
+    stw r9, 4(sp)
 
     movia r8, ADDR_JP1
     movia r9, 0x7F557FF            
@@ -49,17 +49,17 @@ motor0_bwd:
 
     movia r9, 0xFFFFFFFE           
     stwio r9, JP1_DATA(r8)
-	
-	ldw r9, 4(sp)
-	ldw r8, 0(sp)
-	addi sp, sp, 8
+    
+    ldw r9, 4(sp)
+    ldw r8, 0(sp)
+    addi sp, sp, 8
 	
     ret
 
 motor1_fwd:
-	subi sp, sp, 8
-	stw r8, 0(sp)
-	stw r9, 4(sp)
+    subi sp, sp, 8
+    stw r8, 0(sp)
+    stw r9, 4(sp)
 
     movia r8, ADDR_JP1
     movia r9, 0x7F557FF            
@@ -67,17 +67,17 @@ motor1_fwd:
 
     movia r9, 0xFFFFFFF3           
     stwio r9, JP1_DATA(r8)
-	
-	ldw r9, 4(sp)
-	ldw r8, 0(sp)
-	addi sp, sp, 8
+    
+    ldw r9, 4(sp)
+    ldw r8, 0(sp)
+    addi sp, sp, 8
 	
     ret
             
 motor1_bwd:
-	subi sp, sp, 8
-	stw r8, 0(sp)
-	stw r9, 4(sp)
+    subi sp, sp, 8
+    stw r8, 0(sp)
+    stw r9, 4(sp)
 
     movia r8, ADDR_JP1
     movia r9, 0x7F557FF            
@@ -85,9 +85,9 @@ motor1_bwd:
 
     movia r9, 0xFFFFFFFB           
     stwio r9, JP1_DATA(r8)
-	
-	ldw r9, 4(sp)
-	ldw r8, 0(sp)
-	addi sp, sp, 8
+    
+    ldw r9, 4(sp)
+    ldw r8, 0(sp)
+    addi sp, sp, 8
 	
     ret
