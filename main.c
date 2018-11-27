b63c2e75bea3d57af9e1a5e74c29f87cef50865d
Michael <michael.wesolowski@mail.utoronto.ca>
2018-11-27 17:15:18 -0500

    Update main.c

--- main.c
+++ main.c
@@ -13,22 +13,47 @@ int main() {
     setup_interrupts();
     while(1) {
         PS2_data = *(PS2_ptr);	// read the Data register in the PS/2 port
-		RVALID = (PS2_data & 0x8000);	// extract the RVALID field
-		if (RVALID != 0)
-		{
-			/* always save the last three bytes received */
-			byte1 = byte2;
-			byte2 = byte3;
-			byte3 = PS2_data & 0xFF;
-		}
-		if (byte3 == 0xAA)
-		{
-			// mouse inserted; initialize sending of data
-			*(PS2_ptr) = 0xF4;
-		}
-		// Display last byte on Red LEDs
-		*RLEDs = byte3;
-        
+	RVALID = (PS2_data & 0x8000);	// extract the RVALID field
+	if (RVALID != 0)
+	{
+	     /* always save the last three bytes received */
+	     byte1 = byte2;
+	     byte2 = byte3;
+	     byte3 = PS2_data & 0xFF;
+	}
+	if (byte3 == 0xAA)
+	{
+	     // mouse inserted; initialize sending of data
+	     *(PS2_ptr) = 0xF4;
+	}
+	// Display last byte on Red LEDs
+	*RLEDs = byte3;
+
+
+	initialize_timer();
+	start_timer_once();
+	initialize_timer1();
+	start_timer1_continuous();
+	    
+	switch (byte3)
+	{
+		case 0x1C:
+			motor0_fwd();
+			break;
+		case 0x23:
+			motor1_fwd();
+			break;
+		case 0x1D:
+			motor1_bwd();
+			break;
+		case 0x1B:
+			motor0_bwd();
+			break;
+		case 0xF0:
+			motors_off();
+			break;
+	}
+	   
     }
 
     return 0;
